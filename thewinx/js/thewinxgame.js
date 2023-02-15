8af356a7770abf210db6527ce59c9c4fcd4b3925
DanySapiens <dany_sapiens@live.com.mx>
2023-02-15 16:39:26 -0700

    Se integran las alertas para el tipo de hada seleccionada

--- thewinx/js/thewinxgame.js
+++ thewinx/js/thewinxgame.js
@@ -1,11 +1,25 @@
-//aqui va todo el codigo en JavaScript
-function iniciarJuego(){
+//AQUI VA TODO EL CODIGO EN JAVASCRIPT
+
+function iniciarJuego(){ //funcion para iniciar el juego
     let botonHadaJugador = document.getElementById("boton-hada") //llama al elemento de HTML con el Id  
     botonHadaJugador.addEventListener("click",seleccionarHadaJugador) //se ejecuta la funcion cuando se hace click sobre el boton
 }
 
 function seleccionarHadaJugador(){
-    alert("SELECCIONASTE TU HADA")
+    let spanHadaJugador = document.getElementById('hada-jugador')
+
+    if(pyra.checked){
+        alert("Seleccionaste a Pyra")
+    }
+    else if(luna.checked){
+        alert("Seleccionaste a Luna")
+    }
+    else if(fleur.checked){
+        alert("Seleccionaste a Fleur")
+    }else{
+        alert("SELECCIONA UN HADA 🧚🏼‍♂️")
+    }
+
 }
 window.addEventListener('load', iniciarJuego)
 
